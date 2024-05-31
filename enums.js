227b5a6ae54b78259430ea170ebbb89b43971809
AmjadAfzalAhmed <abhutto2006@gmail.com>
2024-05-31 03:27:09 -0700

    task completed

---
+++ enums.js
@@ -0,0 +1,34 @@
+// use of enums with switch statements
+import inquirer from "inquirer";
+import chalk from "chalk";
+var fruits;
+(function (fruits) {
+    fruits["apple"] = "Apple";
+    fruits["mango"] = "Mango";
+    fruits["lemon"] = "Lemon";
+    fruits["pomegranate"] = "Pomegranate";
+})(fruits || (fruits = {}));
+async function fruitUse() {
+    let answers = await inquirer.prompt({
+        name: "select",
+        type: "list",
+        message: chalk.greenBright.italic("\n Select one of the fruits from the list above:"),
+        choices: Object.values(fruits)
+    });
+    switch (answers.select) {
+        case fruits.apple:
+            console.log(chalk.redBright.inverse("\n\t An apple a day keeps the doctor away!!!"));
+            break;
+        case fruits.lemon:
+            console.log(chalk.greenBright.inverse("\n\t Lemon possesses great level of Vitamin C"));
+            break;
+        case fruits.mango:
+            console.log(chalk.yellowBright.inverse("\n\t Mangoes are great for bone health!!!"));
+            break;
+        case fruits.pomegranate:
+            console.log(chalk.red.inverse("\n\t Pomegranates are rich in vitamins and minerals and are good for health"));
+    }
+}
+let fruitList = Object.values(fruits);
+console.log(fruitList);
+await fruitUse();
